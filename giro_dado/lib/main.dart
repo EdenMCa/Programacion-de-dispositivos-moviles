78ce93c114dec4df391b2636c36adeddbfde8e33
EdenMCa <casarrubiaeden@gmail.com>
2025-04-09 13:00:38 -0600

    Se cambia la cara del dado manualmente

--- giro_dado/lib/main.dart
+++ giro_dado/lib/main.dart
@@ -2,36 +2,81 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-              gradient: RadialGradient(
-            colors: [Colors.red, Colors.blue],
-            radius: 1.5,
-          )),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/images/dado1.png",
-                  width: 150,
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeDado(),
+    );
+  }
+}
+
+class HomeDado extends StatefulWidget {
+  const HomeDado({super.key});
+  @override
+  State<HomeDado> createState() {
+    return _HomeDadoState();
+  }
+}
+
+class _HomeDadoState extends State<HomeDado> {
+  var rutaDado = 'assets/images/dado1.png';
+
+  void girarDado(){
+    setState(() {
+      rutaDado = 'assets/images/dado5.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: RadialGradient(
+          colors: [Colors.red, Colors.blue],
+          radius: 1.5,
+        )),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                rutaDado,
+                width: 150,
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                "Cara del dado: 1",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton.icon(
+                onPressed: girarDado,
+                icon: const Icon(Icons.refresh),
+                label: const Text(
+                  "Girar dado",
                 ),
-                const Text(
-                  "Cara del dado: 1", style: TextStyle(fontSize: 20),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 20),
                 ),
-                ElevatedButton.icon(
-                    onPressed: () {
-                      (print("Boton 'Girar' presionado"));
-                    },
-                    icon: const Icon(Icons.refresh),
-                    label: const Text("Girar", style: TextStyle(fontSize: 20),))
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
